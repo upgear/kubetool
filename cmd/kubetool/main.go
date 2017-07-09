aa7b3fd6efd548132e68aa542fbcc75c8139816d
Nick Stogner <nicholas.stogner@gmail.com>
2017-07-09 11:35:41 -0400

    Edit usage

--- cmd/kubetool/main.go
+++ cmd/kubetool/main.go
@@ -20,6 +20,9 @@ func main() {
 		},
 	}
 
+	flag.Usage = func() {
+		fmt.Fprintln(os.Stderr, usage(""))
+	}
 	flag.BoolVar(&input.Flags.Verbose, "v", false, "Log a bunch of stuff")
 	flag.Parse()
 
@@ -59,21 +62,26 @@ func fatal(err error) {
 }
 
 func usage(msg string) string {
-	return msg + "\n\n" +
+	if msg != "" {
+		msg = msg + "\n\n"
+	}
+	return msg +
 		`Usage: kubetool [Options...] <command> <name>
 
 Commands:
     build   Runs docker build
+	push    Runs docker push
     deploy  Runs docker build & kubectl apply
 
 Options:
-    -v      Verbose output
+    -h --help  Print usage
+    -v         Verbose output
 
 Environment Variables:
-    KT_DOCKERFILE_PATH   Directory to look for Dockerfiles
-    KT_KUBERNETES_PATH   Direcotry to look for kubernetes configs
-    KT_DOCKER_TAG        Template to create docker tag
-    KT_DOCKER_CONTEXT    Docker build context (directory)
-    KT_CLOUD             Cloud provider (only supports 'gcloud')
+    KT_DOCKERFILE_PATH  Directory to look for Dockerfiles
+    KT_KUBERNETES_PATH  Direcotry to look for kubernetes configs
+    KT_DOCKER_TAG       Template to create docker tag
+    KT_DOCKER_CONTEXT   Docker build context (directory)
+    KT_CLOUD            Cloud provider (only supports 'gcloud')
 `
 }
