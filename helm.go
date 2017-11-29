a5cee825dc784015217e26f15d5928a3ba01224e
Nick Stogner <nicholas.stogner@gmail.com>
2017-11-29 11:51:43 +0000

    Update helm cmds to not require env configs

--- helm.go
+++ helm.go
@@ -1,23 +1,56 @@
 package kubetool
 
+import "os"
+
 func Install(in CommandInput) (err error) {
-	_, err = cmd(in.Flags.Verbose, "helm", "install", in.HelmChartPath,
+	args := []string{
+		"install", in.HelmChartPath,
 		"--name", in.Release,
 		"--values", in.HelmBaseValueFile,
-		"--values", in.HelmEnvValueFile,
-	)
+	}
+
+	if err := appendEnvConfig(in, &args); err != nil {
+		return err
+	}
+
+	_, err = cmd(in.Flags.Verbose, "helm", args...)
 	return
 }
 
 func Upgrade(in CommandInput) (err error) {
-	_, err = cmd(in.Flags.Verbose, "helm", "upgrade", in.Release, in.HelmChartPath,
+	args := []string{
+		"upgrade", in.Release, in.HelmChartPath,
 		"--values", in.HelmBaseValueFile,
-		"--values", in.HelmEnvValueFile,
-	)
+	}
+
+	if err := appendEnvConfig(in, &args); err != nil {
+		return err
+	}
+
+	_, err = cmd(in.Flags.Verbose, "helm", args...)
+
 	return
 }
 
 func Delete(in CommandInput) (err error) {
 	_, err = cmd(in.Flags.Verbose, "helm", "delete", in.Component.Release)
 	return
+}
+
+// appendEnvConfig is the file exists.
+func appendEnvConfig(in CommandInput, args *[]string) error {
+	_, err := os.Stat(in.HelmEnvValueFile)
+
+	if os.IsNotExist(err) {
+		// The env config file does not exist.
+		return nil
+	}
+
+	if err == nil {
+		// The env config file exists.
+		*args = append(*args, "--values", in.HelmEnvValueFile)
+	}
+
+	// Something went wrong when trying to stat.
+	return err
 }
